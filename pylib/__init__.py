e08dab68b865fb349590d7c889bc35e18eef9290
Eric Busboom <eric@civicknowledge.com>
2021-02-20 07:01:26 -0800

    Initial Commit

---
+++ pylib/__init__.py
@@ -0,0 +1,50 @@
+""" Example pylib functions"""
+
+
+def combined(resource, doc, env, *args, **kwargs):
+    """ An example row generator function.
+
+    Reference this function in a Metatab file as the value of a Datafile:
+
+            Datafile: python:pylib#row_generator
+
+    The function must yield rows, with the first being headers, and subsequenct rows being data.
+
+    :param resource: The Datafile term being processed
+    :param doc: The Metatab document that contains the term being processed
+    :param args: Positional arguments passed to the generator
+    :param kwargs: Keyword arguments passed to the generator
+    :return:
+
+
+    The env argument is a dict with these environmental keys:
+
+    * CACHE_DIR
+    * RESOURCE_NAME
+    * RESOLVED_URL
+    * WORKING_DIR
+    * METATAB_DOC
+    * METATAB_WORKING_DIR
+    * METATAB_PACKAGE
+
+    It also contains key/value pairs for all of the properties of the resource.
+
+    """
+
+    from collections import OrderedDict
+    from copy import copy
+
+    cols = [c['name'] for c in resource.columns() if c['name'] != 'year']
+
+    for r in doc.resources():
+        
+        if r.name == 'free_rp_meals':
+            continue
+        
+        if not hasattr(r, 'year'):
+            continue
+        
+        for d in r.iterdict:
+            yield [r.year] + [ d.get(c) for c in cols]
+            
+            
